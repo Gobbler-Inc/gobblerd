cffc3005e785752b9e6c469a376f449a999e7e4a
Alfred Dobradi <alfred.dobradi@digitgaming.com>
2022-11-06 15:58:01 +0000

    feat: fall back to index.html if requested file not found

--- ui/ui.go
+++ ui/ui.go
@@ -3,9 +3,12 @@ package ui
 import (
 	"embed"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/alfreddobradi/go-bb-man/helper"
 )
@@ -35,13 +38,46 @@ func (ah AssetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fs := http.FS(sub)
+
+	path := r.URL.Path
+	log.Printf("Checking if %s exists in embedded fs...", path)
+	if _, err := fs.Open(path); os.IsNotExist(err) {
+		log.Printf("Failed to open %s: %v", path, err)
+
+		// For now I don't see any point in making this more universal but in the future we might want to add more exceptions
+		if filepath.Ext("path") == ".ico" {
+			helper.E(w, http.StatusNotFound)
+			return
+		}
+
+		log.Printf("Falling back to index.html")
+		f, err := fs.Open("index.html")
+		if err != nil {
+			log.Printf("Failed to open fallback file (index.html): %v", err)
+			helper.E(w, http.StatusInternalServerError)
+			return
+		}
+
+		indexContents, err := io.ReadAll(f)
+		if err != nil {
+			log.Printf("Failed to read fallback file (index.html): %v", err)
+			helper.E(w, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(indexContents) // nolint
+		return
+	} else if err != nil {
+		log.Printf("Failed to open %s: %v", path, err)
+		helper.E(w, http.StatusInternalServerError)
+		return
+	}
+
 	http.FileServer(fs).ServeHTTP(w, r)
 }
 
-type SpaHandler struct {
-	staticPath string
-	indexPath  string
-}
+type SpaHandler struct{}
 
 func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	index, err := templates.ReadFile("templates/index.html")
